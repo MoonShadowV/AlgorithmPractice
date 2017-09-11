107c762acc0d2e1a68debc1f7d36d9e19381cb1c
ShadowMoonV <834540085@qq.com>
2017-09-11 22:13:57 +0800

    Day4

---
+++ src/BinaryTree/PrintEdge.java
@@ -0,0 +1,10 @@
+package BinaryTree;
+
+/**
+ * Created by ShadowMoon on 2017/9/11.
+ */
+public class PrintEdge {
+    public void print1(Node head){
+
+    }
+}
--- src/BinaryTree/StackTree.java
+++ src/BinaryTree/StackTree.java
@@ -12,5 +12,60 @@ public class StackTree {
         }
         Stack<Node> stack = new Stack<Node>();
         stack.push(head);
+        while (!stack.isEmpty()){
+            head = stack.pop();
+            print(head);
+            if (head.right != null){
+                stack.push(head.right);
+            }
+            if (head.left != null){
+                stack.push(head.left);
+            }
+        }
+    }
+
+    public void midOrderStack(Node head){
+        if (head == null){
+            return;
+        }
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(head);
+        while (!stack.isEmpty() || head != null){
+            if (head != null){
+                stack.push(head);
+                head = head.left;
+            }else {
+                head = stack.pop();
+                print(head);
+                head = head.right;
+            }
+        }
+    }
+
+    public void posOrderStack(Node head){
+        if (head == null){
+            return;
+        }
+        Stack<Node> s1 = new Stack<Node>();
+        Stack<Node> s2 = new Stack<Node>();
+        s1.push(head);
+        while (!s1.isEmpty()){
+            head = s1.pop();
+            s2.push(head);
+            if (head.left != null){
+                s1.push(head.left);
+            }
+            if (head.left != null){
+                s1.push(head.right);
+            }
+        }
+
+        while (!s2.isEmpty()){
+            print(s2.pop());
+        }
+    }
+
+    private void print(Node node){
+        System.out.print(node.value+" ");
     }
 }
